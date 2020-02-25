5167e5ac70c000fb8779c9a2b6606117fd225dfb
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-25 20:34:30 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,6 +20,13 @@ function whileLoop(number){
 
 
 function doWhileLoop(num){
-  
-  
+  var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+  do{
+    console.log("I run once regardless.");
+  }while (incrementVariable() < num)
 }
